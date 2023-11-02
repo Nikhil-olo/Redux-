48fa415f5189d4b1ad444bb1bf6929ab63fde0e9
Nikhil-olo <83595187+Nikhil-olo@users.noreply.github.com>
2023-11-02 15:09:05 +0530

    user interface ready

---
+++ src/component/shop.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Shop = () => {
+  return (
+    <div>
+    <button className='btn btn-primary'>-</button>
+    Add this item to wish list!
+    <button className='btn btn-primary'>+</button>
+    </div>
+  )
+}
+
+export default Shop;
